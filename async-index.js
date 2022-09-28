acc27425da78719244f91faefd48d7bb6bc122aa
dershah <theshakil21@gmail.com>
2022-09-28 21:51:02 +0200

    update

--- async-index.js
+++ async-index.js
@@ -24,6 +24,7 @@ const writeFilePro = (file, data) => {
 
 //-------------------------------------------------------await/Async------------------------------------------------------
 
+/*
 const getDogPic = async () => {
     try {
         
@@ -43,7 +44,32 @@ const getDogPic = async () => {
     return "2. Ready ('-')";
 
 }
+*/
+//-------------------------------------------------------await/Async (multiple file "Same time")------------------------------------------------------
+const getDogPic = async () => {
+    try {
+        
+        const data = await readFilePro(`${__dirname}/3-asynchronous-JS/starter/dog.txt`);
+        console.log(`Breed: ${data}`);
 
+        const res1Pro = superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+        const res2Pro = superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+        const res3Pro = superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+        
+        const all = await Promise.all([res1Pro, res2Pro, res3Pro]);
+        const imgs = all.map(el => el.body.message);
+        console.log(imgs);
+
+        await writeFilePro(`${__dirname}/3-asynchronous-JS/starter/dog-img.txt`, imgs.join(''));
+        console.log("Random dog image has been saved");
+
+    } catch (err) {
+        console.log(err);
+        throw err;
+    }
+    return "2. Ready ('-')";
+
+}
 (async()=> {
     try {
         console.log("1. Will get the Dog Picture!");
