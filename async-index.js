c932cc44d07b10f8cc3cdf06f10323054c0ab73f
dershah <theshakil21@gmail.com>
2022-09-28 11:33:37 +0200

    New Update

--- async-index.js
+++ async-index.js
@@ -12,18 +12,57 @@ const readFilePro = file => {
         })
     })
 }
+const writeFilePro = (file, data) => {
+    return new Promise((resolve, reject)=>{
+        fs.writeFile(file, data, err=> {
+            if(err) reject("unable to write the file");
 
-readFilePro(`${__dirname}/3-asynchronous-JS/starter/dog.txt`).then(data => {
-    console.log(`Breed: ${data}`);
+            resolve("sucess");
+        })
+    })
+}
 
-    superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).then(res =>{
+//-------------------------------------------------------await/Async------------------------------------------------------
+
+const getDogPic = async () => {
+    try {
+        
+        const data = await readFilePro(`${__dirname}/3-asynchronous-JS/starter/dog.txt`);
+        console.log(`Breed: ${data}`);
+
+        const res = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
         console.log(res.body.message);
 
-        fs.writeFile(`${__dirname}/3-asynchronous-JS/starter/dog-img.txt`, res.body.message, err => {
-            if(err) return console.log(err.message);
-            console.log("random dog image saved into the file");
-        });
-    }).catch(err => {
-        console.log(err.message);
-    });
+        await writeFilePro(`${__dirname}/3-asynchronous-JS/starter/dog-img.txt`, res.body.message);
+        console.log("Random dog image has been saved");
+
+    } catch (err) {
+        console.log(err);
+    }
+    return "2. Ready ('-')";
+
+}
+
+console.log("1. Will get the Dog Picture!");
+
+getDogPic();
+console.log("3. Finish getting the Dog Picture!");
+
+
+
+/* 
+readFilePro(`${__dirname}/3-asynchronous-JS/starter/dog.txt`)
+.then(data => {
+    console.log(`Breed: ${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
 })
+.then(res =>{
+    console.log(res.body.message);
+    return writeFilePro(`${__dirname}/3-asynchronous-JS/starter/dog-img.txt`, res.body.message);
+})
+.then(() =>{
+    console.log("Random dog image has been saved");
+}).catch(err => {
+        console.log(err.message);
+});
+ */
