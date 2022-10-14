0a6843aeb4a0598807550c96981dfa3476a4f940
dershah <theshakil21@gmail.com>
2022-10-14 07:03:55 +0200

    m-changes

--- app.js
+++ app.js
@@ -1,15 +1,49 @@
-const fs = require('fs');
-const express = require('express');
+const fs = require("fs");
+const express = require("express");
+const { stringify } = require("querystring");
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.status(200).json({message: 'saying Hi from Server Side', app: 'Natours'});
-});
-app.post('/', (req, res) => {
-    res.status(200).send('To Post use this endpöoint');
-});
 
+/* app.get("/", (req, res)=> {
+    res.status(200).json({
+        massage: "Again Hello from the server Side", app: "natour"
+    });
+});
+app.post("/post", (req, res)=> {
+    res.send("For posting use this endpoint...");
+});
+ */
+
+const toursData = JSON.parse(fs.readFileSync(`${__dirname}/4-natours/after-section-06/dev-data/data/tours-simple.json`));
+
+app.get("/api/v1/tours", (req, res) => {
+     res.status(200). json({
+        status: "sucess",
+        result: toursData.length,
+        data:{
+            tours: toursData
+        }
+     }) 
+})
+
+app.post("/api/v1/tours", (req, res) => {
+    //console.log(req.body);
+
+    const newId = toursData.length;
+    //toursData[toursData.length-1].id + 1;
+    const newTours= Object.assign({id: newId}, req.body);
+    toursData.push(newTours);
+    fs.writeFile(`${__dirname}/4-natours/after-section-06/dev-data/data/tours-simple.json`, JSON.stringify(toursData), err =>{
+        res.status(201).json({
+            status: "sucess", 
+            data: {
+                tours: newTours
+            }
+        })
+    })
+})
 
 const port = 3000;
 app.listen(port, () => {
